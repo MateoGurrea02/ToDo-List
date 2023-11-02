ba813cb705d12fe0b47130c9e233f8839d7a3a1a
Mateo Gurrea <mategurrea@gmail.com>
2023-11-02 18:28:43 -0300

    creadndo inicio de sesion con ruta

---
+++ ToDo-List/src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export default function Menu(){
+    return(
+      <div>
+        <button>
+            <Link to='/'> Home</Link>
+        </button>
+      </div>
+    )
+  }
+  
