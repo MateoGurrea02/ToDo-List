162b9bc50c47ac3ee23b80c3afb03b3c7d7c4780
Mateo Gurrea <mategurrea@gmail.com>
2023-11-08 16:43:37 -0300

    fix

--- ToDo-List/src/main.jsx
+++ ToDo-List/src/main.jsx
@@ -29,7 +29,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Menu></Menu>
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
